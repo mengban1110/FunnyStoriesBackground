74a1efd2f8ba8f80b0f6d9adf5dc6b2990521dd6
mengban1110 <50842998+mengban1110@users.noreply.github.com>
2021-04-07 11:01:00 +0800

    修改 查看帖子评论 每页个数

--- js/postscontroller/Comment.js
+++ js/postscontroller/Comment.js
@@ -122,7 +122,7 @@ function delComment(no) {
 }
 
 function pagezz(){
-	var size = 1
+	var size = 10
 	mypost(getPostCommentCount, {postid:GetQueryString("postid")}, function(data) {
 		console.log("获取总用户数")
 		console.log(data)
@@ -181,4 +181,19 @@ function pageword(){
 	}else{
 		pagezz()
 	}
+}
+
+
+function add0(m){return m<10?'0'+m:m }
+function format(shijianchuo)
+{
+//shijianchuo是整数，否则要parseInt转换
+var time = new Date(shijianchuo);
+var y = time.getFullYear();
+var m = time.getMonth()+1;
+var d = time.getDate();
+var h = time.getHours();
+var mm = time.getMinutes();
+var s = time.getSeconds();
+return y+'-'+add0(m)+'-'+add0(d)+' '+add0(h)+':'+add0(mm)+':'+add0(s);
 }
